7274cc2232b9669b61feea09cf34bfd56369260f
Melissa <melissa.palermo@hotmail.com>
2016-12-04 23:22:57 -0200

    skew slah está em looping

--- lab que roda.R
+++ lab que roda.R
@@ -217,10 +217,10 @@ Lambda = function(i){
     ynovo[d]=c
     fit=smsn.nl(y=ynovo,x=x,betas=c(1.5,2.5), 
                 sigma2=.25,shape =.75, nlf = function(x, betas){betas[1]+betas[2]*x}, 
-                criteria = TRUE,family = "Skew.slash",iter.max = 300)
+                criteria = TRUE,family = "Skew.slash",iter.max = 300, nu=2.6)
     fity=smsn.nl(y=y,x=x,betas=c(1.5,2.5), 
                  sigma2=.25,shape =.75, nlf = function(x, betas){betas[1]+betas[2]*x}, 
-                 criteria = TRUE,family = "Skew.slash",iter.max = 300)
+                 criteria = TRUE,family = "Skew.slash",iter.max = 300, nu=2.6)
     vetaic=c(vetaic,fit$AIC )
     vetbic=c(vetbic,fit$BIC )
     vetbeta1=c(vetbeta1,fit$betas[1] )
@@ -275,11 +275,97 @@ RC_b1=c(vet1[5], vet2[5], vet3[5], vet4[5], vet5[5], vet6[5], vet7[5], vet8[5]
 RC_b2=c(vet1[6], vet2[6], vet3[6], vet4[6], vet5[6], vet6[6], vet7[6], vet8[6]
         , vet9[6], vet10[6], vet11[6], vet12[6], vet13[6], vet14[6], vet15[6])
 
-plot(v,aic, ylab="AIC sem contaminação", xlab="Lambda", main="AIC's Skew t")
-plot(v,bic, ylab="BIC sem contaminação", xlab="Lambda", main="BIC's Skew t")
-plot(v,aic_c, ylab="AIC com contaminação", xlab="Lambda", main="AIC's Skew t")
-plot(v,bic_c, ylab="BIC com contaminação", xlab="Lambda", main="BIC's Skew t")
+plot(v,aic, ylab="AIC sem contaminação", xlab="Lambda", main="AIC's Skew Slash")
+plot(v,bic, ylab="BIC sem contaminação", xlab="Lambda", main="BIC's Skew Slash")
+plot(v,aic_c, ylab="AIC com contaminação", xlab="Lambda", main="AIC's Skew Slash")
+plot(v,bic_c, ylab="BIC com contaminação", xlab="Lambda", main="BIC's Skew Slash")
 
 ################################################################################
-#######################SNC########################################
+#######################SSkew CN########################################
+
+Lambda = function(i){
+  vetaic=NULL
+  vetbic=NULL
+  vetbeta1=NULL
+  vetbeta2=NULL
+  
+  vetaicy=NULL
+  vetbicy=NULL
+  vetbeta1y=NULL
+  vetbeta2y=NULL
+  
+  rcb1=NULL
+  rcb2=NULL
+  for(j in 1:100){
+    
+    e<-SkewNormal(100, 0.5, 1.5)
+    y= betas[1]+betas[2]*x+e
+    d=sample(1:100, 3, replace=FALSE, prob=NULL)
+    c=(1+(v[i]/100))*y[d]
+    ynovo=y
+    ynovo[d]=c
+    fit=smsn.nl(y=ynovo,x=x,betas=c(1.5,2.5), 
+                sigma2=.25,shape =.75, nlf = function(x, betas){betas[1]+betas[2]*x}, 
+                criteria = TRUE,family = "Skew.cn",iter.max = 300, nu=c(0.1,0.1))
+    fity=smsn.nl(y=y,x=x,betas=c(1.5,2.5), 
+                 sigma2=.25,shape =.75, nlf = function(x, betas){betas[1]+betas[2]*x}, 
+                 criteria = TRUE,family = "Skew.cn",iter.max = 300, nu=c(0.1,0.1))
+    vetaic=c(vetaic,fit$AIC )
+    vetbic=c(vetbic,fit$BIC )
+    vetbeta1=c(vetbeta1,fit$betas[1] )
+    vetbeta2=c(vetbeta2,fit$betas[2] )
+    
+    vetaicy=c(vetaicy,fity$AIC )
+    vetbicy=c(vetbicy,fity$BIC )
+    vetbeta1y=c(vetbeta1y,fity$betas[1] )
+    vetbeta2y=c(vetbeta2y,fity$betas[2] )
+    
+    rcb1=c(rcb1, abs((fity$betas[1]-fit$betas[1])/fity$betas[1])*100)
+    rcb2=c(rcb2, abs((fity$betas[2]-fit$betas[2])/fity$betas[2])*100)
+    
+    
+  }
+  lambda =c(mean(vetaicy), mean(vetbicy), mean(vetaic), mean(vetbic), mean(rcb2), mean(rcb1))
+  
+  return(lambda)
+}
+
+vet1=Lambda(1)
+vet2=Lambda(2)
+vet3=Lambda(3)
+vet4=Lambda(4)
+vet5=Lambda(5)
+vet6=Lambda(6)
+vet7=Lambda(7)
+vet8=Lambda(8)
+vet9=Lambda(9)
+vet10=Lambda(10)
+vet11=Lambda(11)
+vet12=Lambda(12)
+vet13=Lambda(13)
+vet14=Lambda(14)
+vet15=Lambda(15)
+
+aic=c(vet1[1], vet2[1], vet3[1], vet4[1], vet5[1], vet6[1], vet7[1], vet8[1]
+      , vet9[1], vet10[1], vet11[1], vet12[1], vet13[1], vet14[1], vet15[1])
+
+bic=c(vet1[2], vet2[2], vet3[2], vet4[2], vet5[2], vet6[2], vet7[2], vet8[2]
+      , vet9[2], vet10[2], vet11[2], vet12[2], vet13[2], vet14[2], vet15[2])
+
+aic_c=c(vet1[3], vet2[3], vet3[3], vet4[3], vet5[3], vet6[3], vet7[3], vet8[3]
+        , vet9[3], vet10[3], vet11[3], vet12[3], vet13[3], vet14[3], vet15[3])
+
+bic_c=c(vet1[4], vet2[4], vet3[4], vet4[4], vet5[4], vet6[4], vet7[4], vet8[4]
+        , vet9[4], vet10[4], vet11[4], vet12[4], vet13[4], vet14[4], vet15[4])
+
+RC_b1=c(vet1[5], vet2[5], vet3[5], vet4[5], vet5[5], vet6[5], vet7[5], vet8[5]
+        , vet9[5], vet10[5], vet11[5], vet12[5], vet13[5], vet14[5], vet15[5])
+
+RC_b2=c(vet1[6], vet2[6], vet3[6], vet4[6], vet5[6], vet6[6], vet7[6], vet8[6]
+        , vet9[6], vet10[6], vet11[6], vet12[6], vet13[6], vet14[6], vet15[6])
+
+plot(v,aic, ylab="AIC sem contaminação", xlab="Lambda", main="AIC's Skew CN")
+plot(v,bic, ylab="BIC sem contaminação", xlab="Lambda", main="BIC's Skew CN")
+plot(v,aic_c, ylab="AIC com contaminação", xlab="Lambda", main="AIC's Skew CN")
+plot(v,bic_c, ylab="BIC com contaminação", xlab="Lambda", main="BIC's Skew CN")
 
